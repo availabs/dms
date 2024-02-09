2757f353c5eda57460d75cd3a1aa534af98a80fc
shaunaksangdod <shaunaksangdod@gmail.com>
2024-02-09 14:44:25 -0500

    forms as pattern

---
+++ src/patterns/forms/forms.format.js
@@ -0,0 +1,17 @@
+export const formsConfigFormat = {
+    app: "dms-site",
+    type: "forms-config",
+    attributes: [
+        {
+            key: 'name',
+            label: 'Name',
+            type: 'text'
+        },
+        {
+            key: 'config',
+            label: 'Config',
+            prompt: 'Paste full config here.',
+            type: 'textarea'
+        },
+    ]
+}
