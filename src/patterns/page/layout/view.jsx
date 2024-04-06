ef8c1d3d3c124a9ef6d8207a336d96795d3ea5d4
alex <am3081@gmail.com>
2024-04-06 15:59:13 -0400

    simplify theme and for page pattern view and edit

--- src/patterns/page/layout/view.jsx
+++ src/patterns/page/layout/view.jsx
@@ -24,76 +24,40 @@ function PageView ({item, dataItems, attributes, logo, rightMenu}) {
     return items
   }, [dataItems])
 
-  console.log('page view', item.template_id)
-
   const level = item?.index === '999' ? 1 : detectNavLevel(dataItems, baseUrl);
   const inPageNav = getInPageNav(item);
-
-  //console.log('page view', item.template_id, level)
 
   const headerSection = item['sections']?.filter(d => d.is_header)?.[0]
   const sections = item['sections']?.filter(d => !d.is_header && !d.is_footer)
   
   return (
-    <div>
-    {/* Header */}
-    {(item?.header === 'above') && <ContentView item={item} value={[headerSection]} {...attributes['sections']}/>}
-    {/* Layout */}
-    <Layout 
-      topNav={{menuItems, position: 'fixed', logo, rightMenu }}
-      sideNav={item.sideNav ? item.sideNav : inPageNav}
-    >
-      <div className={`${theme.layout.page} ${theme.navPadding[level]}`}>
-        {(item?.header === 'below') && <ContentView item={item} value={[headerSection]} {...attributes['sections']}/>}
-        <div className={theme.layout.container}>
-          {/*----Page ---*/}
-          <div className='flex flex-1 h-full w-full px-1 md:px-6 py-6'>
-            {/*<div className='w-[264px]' />*/}
-            {item?.sidebar === 'show' ? 
-                (<div className='w-64 hidden xl:block'>
-                  <div className='w-64 sticky top-20 hidden xl:block h-screen'> 
-                    <div className='h-[calc(100%_-_5rem)] overflow-y-auto overflow-x-hidden font-display'>
-                      <SideNav {...inPageNav} /> 
-                    </div>
-                  </div>
-                </div>)
-              : ''}
-            
-            <div className={theme.page.content}>
-              <div className={theme.page.container + ' '}>
-                {(item?.header === 'inpage') && <ContentView item={item} value={[headerSection]} {...attributes['sections']}/>}
-    
-                {/*<div className='px-6 py-4 font-sans font-medium text-xl text-slate-700 uppercase max-w-5xl mx-auto'>
-                  {item['title']}
-                </div>*/}
-                
-                <div className='text-md font-light leading-7'>
-                  {user?.authLevel >= 5 ?  
-                    <div className='w-full flex relative'>
-                      <div className='flex-1' />
-                      <Link className='absolute -right-[10px] -top-[13px]' to={`${baseUrl}/edit/${item.url_slug}`}>
-                        <i className={`fad fa-edit fa-fw flex-shrink-0 text-lg text-slate-400 hover:text-blue-500`}/>
-                      </Link> 
-                    </div>
-                    : ''    
-                  }
-                  
-                  <ContentView 
-                    item={item}
-                    value={sections} 
-                    {...attributes['sections']}
-                  />
-                </div>
-              </div>    
-            </div>
+    <div id='page_view'>
+      {/* Header */}
+      {(item?.header === 'above') && <ContentView item={item} value={[headerSection]} {...attributes['sections']}/>}
+      {/* Layout */}
+      <Layout 
+        topNav={{menuItems, position: 'fixed', logo, rightMenu }}
+        sideNav={item.sideNav ? item.sideNav : inPageNav}
+      >
+        <div className={`${theme.page.wrapper1} ${theme.navPadding[level]}`}>
+          {(item?.header === 'below') && <ContentView item={item} value={[headerSection]} {...attributes['sections']}/>}
+          <div className={`${theme.page.wrapper2}`}>
+            {item?.sidebar === 'show' && <RenderSideNav inPageNav={inPageNav} />}      
+            <div className={theme.page.wrapper3}>
+              {/* Content */}
+              {(item?.header === 'inpage') && <ContentView item={item} value={[headerSection]} {...attributes['sections']}/>}
+              {user?.authLevel >= 5 && <ToggleEdit item={item} baseUrl={baseUrl} />}
+              <ContentView 
+                item={item}
+                value={sections} 
+                {...attributes['sections']}
+              />
+            </div>    
           </div>
-          {/*---- END Page ---*/}
-          
         </div>
-      </div>
-    </Layout>
-    {/*Footer*/}
-    {item?.footer && <div className='h-[300px] bg-slate-100' />}
+      </Layout>
+      {/*Footer*/}
+      {item?.footer && <div className='h-[300px] bg-slate-100' />}
     </div>
   ) 
 }
@@ -101,5 +65,23 @@ function PageView ({item, dataItems, attributes, logo, rightMenu}) {
 
 export default PageView
 
-
+function ToggleEdit({baseUrl, item}) {
+  return (
+    <Link className='z-30 absolute right-[10px] top-[5px]' to={`${baseUrl}/edit/${item.url_slug}`}>
+      <i className={`fad fa-edit fa-fw flex-shrink-0 text-lg text-slate-400 hover:text-blue-500`}/>
+    </Link> 
+  )
+}
+ 
+function RenderSideNav({inPageNav}) {
+  return (
+    <div className='w-64 hidden xl:block'>
+      <div className='w-64 sticky top-20 hidden xl:block h-screen'> 
+        <div className='h-[calc(100%_-_5rem)] overflow-y-auto overflow-x-hidden font-display'>
+          <SideNav {...inPageNav} /> 
+        </div>
+      </div>
+    </div>
+  )
+}
 
