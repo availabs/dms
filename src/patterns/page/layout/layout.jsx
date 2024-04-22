e2abdcdc63cd04ac0b2dc9e37482d4b6525ddf82
alex <am3081@gmail.com>
2024-04-22 13:04:59 -0400

    cool

--- src/patterns/page/layout/layout.jsx
+++ src/patterns/page/layout/layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {CMSContext} from '../siteConfig'
+import { useFalcor } from '@availabs/avl-falcor';
 
 // for instances without auth turned on can edit
 // should move this to dmsFactory default authWrapper
 const defaultUser = { email: "user", authLevel: 5}
 
-export default function SiteLayout ({children, theme, baseUrl='', useFalcor, user=defaultUser, pgEnv, ...props}) {
+export default function SiteLayout ({children, theme, baseUrl='', user=defaultUser, pgEnv, ...props}) {
   const { falcor, falcorCache } = useFalcor();
   return (
     <CMSContext.Provider value={{baseUrl, user, theme, falcor, falcorCache, pgEnv}}>
