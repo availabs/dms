a9d474961a53f922458df065b19ec90f896a1af1
shaunaksangdod <shaunaksangdod@gmail.com>
2024-09-10 16:51:10 -0400

    kinds in a good place

--- src/patterns/page/components/saveAsPDF/PrintWell/selectablePDF.js
+++ src/patterns/page/components/saveAsPDF/PrintWell/selectablePDF.js
@@ -1,14 +1,29 @@
 import {API_HOST} from "../../../../../../../../config";
-
-
+import {style} from "./style_str"
 export const selectablePDF = async (pdfRef) => {
+    const html = `
+                        <!DOCTYPE html>
+                            <html lang="en">
+                            <head>
+                                <title>PDF</title>
+                                <meta name="viewport" content="width=1600, initial-scale=1" />
+                                <link rel="stylesheet" type="text/css" href="https://mitigateny.org/css/build.css" />
+                                <style>
+                                  @import url('https://fonts.googleapis.com/css2?family=Figtree:wght@400;500;600;700;800;900&family=IBM+Plex+Sans:wght@100;200;300;400;500;600;700&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Oswald:wght@300;500;700&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&family=Rubik:wght@300;400;500;600;700;800&family=Work+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap');
+                                </style>
+                            </head>
+                            <body style="padding:30px;width: 1213px">
+                                ${pdfRef.current.innerHTML}
+                            </body>
+                            </html>`;
+    console.log(html)
    const response = await fetch(`${API_HOST}/dama-admin/hazmit_dama/downloadpdf`,
        {
           method: 'POST',
           headers: {
              'Content-Type': 'text/html',
           },
-          body: pdfRef.current.innerHTML,
+          body: html,
        });
    console.log('res', response)
    if (response.ok) {
