2f00f5909e7d1093225cafa0acf16389fbe73c9a
shaunaksangdod <shaunaksangdod@gmail.com>
2024-12-12 12:16:30 -0500

    pages page

--- src/patterns/page/pages/manager/pages.jsx
+++ src/patterns/page/pages/manager/pages.jsx
@@ -90,12 +90,15 @@ const getExpandableItems = (items) => items.reduce((acc, curr) => curr.children 
 // ==================================================== util fns end ===================================================
 
 const Pill = ({color, text}) => {
-    const colors = `bg-${color}-500/15 text-${color}-700 group-data-[hover]:bg-${color}-500/25`;
+    const colors = {
+        orange: `bg-orange-500/15 text-orange-700 group-data-[hover]:bg-orange-500/25`,
+        blue: `bg-blue-500/15 text-blue-700 group-data-[hover]:bg-blue-500/25`
+    };
     return (
         <div
             className="group relative inline-flex rounded-md focus:outline-none">
             <span className={`inline-flex items-center gap-x-1.5 rounded-md px-1.5 py-0.5 text-sm/5 font-medium sm:text-xs/5 forced-colors:outline
-            ${colors}
+            ${colors[color]}
             `}>{text}</span>
         </div>
     )
