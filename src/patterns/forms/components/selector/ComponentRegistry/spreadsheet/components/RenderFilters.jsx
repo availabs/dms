83cbf8042ab7df6bedded892a49cb8c053cbbe55
shaunaksangdod <shaunaksangdod@gmail.com>
2025-01-13 11:07:30 -0500

    minor filter update

--- src/patterns/forms/components/selector/ComponentRegistry/spreadsheet/components/RenderFilters.jsx
+++ src/patterns/forms/components/selector/ComponentRegistry/spreadsheet/components/RenderFilters.jsx
@@ -1,19 +1,14 @@
 import React, {useCallback, useEffect, useMemo, useState} from "react";
-import {useNavigate, useSearchParams} from "react-router-dom";
 import {dmsDataTypes} from "../../../../../../../data-types";
 import {formattedAttributeStr, attributeAccessorStr, convertToUrlParams} from "../utils/utils";
 import {Filter} from "../../../../../../admin/ui/icons";
 import {isJson} from "../../../index";
 import {uniqBy} from "lodash-es"
 
-export const getData = async ({format, apiLoad, length, attributes, allAttributes, groupBy=[], filterBy={}}) =>{
-    // fetch all data items based on app and type. see if you can associate those items to its pattern. this will be useful when you have multiple patterns.
-    const finalAttributes = attributes || (
-        isJson(format?.config) ? (format.config?.attributes || []) :
-            (JSON.parse(format?.config || '{}')?.attributes || format?.metadata?.columns || [])
-    );
-
-    const {name, display, meta_lookup} = allAttributes.find(attr => attr.name === attributes[0]) || {};
+export const getData = async ({format, apiLoad, length, attribute, allAttributes, groupBy=[], filterBy={}}) =>{
+    const mappedAttributeName = attribute.toLowerCase().startsWith('distinct') ? attribute : `distinct ${attribute} as ${attribute}`; // to get uniq values
+    // const attributeNameForExclude = attribute.toLowerCase().be
+    const {name, display, meta_lookup} = allAttributes.find(attr => attr.name === attribute) || {};
     const meta = ['meta-variable', 'geoid-variable', 'meta'].includes(display) && meta_lookup ? {[name]: meta_lookup} : {};
     const fromIndex = 0;
     const toIndex = length-1;
@@ -25,8 +20,14 @@ export const getData = async ({format, apiLoad, length, attributes, allAttribute
         filter: {
             fromIndex: path => fromIndex,
             toIndex: path => toIndex,
-            options: JSON.stringify({groupBy, aggregatedLen: groupBy.length, filter: filterBy, exclude: {[groupBy[0]]: ['null']}, meta, keepOriginalValues: true}),
-            attributes: finalAttributes,
+            options: JSON.stringify({
+                // groupBy,
+                filter: filterBy,
+                // exclude: {[attribute]: ['null']},
+                meta,
+                keepOriginalValues: true
+            }),
+            attributes: [mappedAttributeName],
             stopFullDataLoad: true
         },
     }]
@@ -34,10 +35,10 @@ export const getData = async ({format, apiLoad, length, attributes, allAttribute
         app: format.app,
         type: format.type,
         format,
-        attributes: finalAttributes,
+        attributes: [mappedAttributeName],
         children
     });
-    return data;
+    return data.map(row => ({[attribute]: row[mappedAttributeName]}));
 }
 
 export const getLength = async ({format, apiLoad, groupBy= [], filterBy}) =>{
@@ -75,7 +76,8 @@ const parseIfJson = value => {
         return value;
     }
 }
-export const RenderFilters = ({attributes, filters, setFilters, format, apiLoad}) => {
+export const RenderFilters = ({attributes, filters, setFilters, format, defaultOpen=false, apiLoad}) => {
+    const [open, setOpen] = useState(defaultOpen);
     const [filterOptions, setFilterOptions] = useState({}); // {col1: [vals], col2:[vals]}
     // console.log('render filters props', format, attributes)
     // filters don't work for newly added values. this has started happening after accommodating multiselect filters using valueSets. can be solved by using merge of values and valuesets in format filters.
@@ -113,9 +115,8 @@ export const RenderFilters = ({attributes, filters, setFilters, format, apiLoad}
                     format: {...format, type: format.doc_type},
                     apiLoad,
                     length,
-                    attributes: [getFormattedAttributeStr(filter.column, format.isDms, attributes)],
+                    attribute: getFormattedAttributeStr(filter.column, format.isDms, attributes),
                     allAttributes: attributes,
-                    groupBy: [getAttributeAccessorStr(filter.column, format.isDms, attributes)],
                     filterBy
                 })
                 debug && console.log('debug filters: data', data, acc)
@@ -157,43 +158,49 @@ export const RenderFilters = ({attributes, filters, setFilters, format, apiLoad}
     const MultiSelectComp = dmsDataTypes.multiselect.EditComp;
     if(!filters.length || !attributes.length) return null;
     return (
-        <div className={'p-4 flex flex-col border border-blue-300 rounded-md'}>
-            <Filter className={'-mt-4 -mr-6 text-blue-300 bg-white self-end rounded-md'}/>
-            {filters.map((f, i) => (
-                <div className={'w-full flex flex-row items-center'}>
-                    <div className={'w-1/4 p-1 text-sm'}>
-                        {attributes.find(attr => attr.name === f.column)?.display_name || f.column}
+        open ?
+            <div className={'p-4 flex flex-col border border-blue-300 rounded-md'}>
+                <Filter className={'-mt-4 -mr-6 text-blue-300 bg-white self-end rounded-md hover:cursor-pointer'}
+                        onClick={() => setOpen(false)}/>
+                {filters.map((f, i) => (
+                    <div className={'w-full flex flex-row items-center'}>
+                        <div className={'w-1/4 p-1 text-sm'}>
+                            {attributes.find(attr => attr.name === f.column)?.display_name || f.column}
+                        </div>
+                        <div className={'w-3/4 p-1 relative'}>
+                            <MultiSelectComp
+                                className={`max-h-[150px] flex text-xs overflow-auto scrollbar-sm border rounded-md bg-white ${f.values?.length ? `p-1` : `p-4`}`}
+                                placeholder={'Search...'}
+                                value={f.values}
+                                onChange={e => {
+                                    const newValues = (e || []).map(filterItem => filterItem?.value || filterItem);
+                                    const newFilters =
+                                        filters.map((filter, fI) => fI === i ?
+                                            {
+                                                ...f,
+                                                values: e,
+                                                valueSets:
+                                                    (filterOptions?.[f.column]?.allValues || [])
+                                                        .filter(v => {
+                                                            const parsedValueSet = parseIfJson(v);
+                                                            return newValues.some(e1 => Array.isArray(parsedValueSet) ? v.includes(e1) : v === e1)
+                                                        })
+                                                // .filter(v => Array.isArray(v) ? v.some(v1 => e.includes(v1)) : e.includes(v)).map(v => `[${v.map(v1 => `"${v1}"`)}]`)
+                                            } : filter);
+                                    // const url = `?${convertToUrlParams(newFilters, delimiter)}`;
+                                    console.log('new filters', newFilters)
+                                    setFilters(newFilters)
+                                    // navigate(url)
+                                }}
+                                options={filterOptions?.[f.column]?.uniqValues || []}
+                                displayInvalidMsg={false}
+                            />
+                        </div>
                     </div>
-                    <div className={'w-3/4 p-1 relative'}>
-                        <MultiSelectComp
-                            className={`max-h-[150px] flex text-xs overflow-auto scrollbar-sm border rounded-md bg-white ${f.values?.length ? `p-1` : `p-4`}`}
-                            placeholder={'Search...'}
-                            value={f.values}
-                            onChange={e => {
-                                const newValues = (e || []).map(filterItem => filterItem?.value || filterItem);
-                                const newFilters =
-                                    filters.map((filter, fI) => fI === i ?
-                                        {...f,
-                                            values: e,
-                                            valueSets:
-                                                (filterOptions?.[f.column]?.allValues || [])
-                                                    .filter(v => {
-                                                        const parsedValueSet = parseIfJson(v);
-                                                        return newValues.some(e1 => Array.isArray(parsedValueSet) ? v.includes(e1) : v === e1)
-                                                    })
-                                                    // .filter(v => Array.isArray(v) ? v.some(v1 => e.includes(v1)) : e.includes(v)).map(v => `[${v.map(v1 => `"${v1}"`)}]`)
-                                        } : filter);
-                                // const url = `?${convertToUrlParams(newFilters, delimiter)}`;
-                                console.log('new filters', newFilters)
-                                setFilters(newFilters)
-                                // navigate(url)
-                            }}
-                            options={filterOptions?.[f.column]?.uniqValues || []}
-                            displayInvalidMsg={false}
-                        />
-                    </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div> :
+            <div className={'px-4 flex flex-col'}>
+                <Filter className={'-mr-6 text-blue-300 bg-white self-end rounded-md hover:cursor-pointer'} onClick={() => setOpen(true)}/>
+            </div>
     )
 }
