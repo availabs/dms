394491e5c87b10bbbf4f95568bddf17e81bff216
Alex Muro <am3081@gmail.com>
2025-02-10 16:01:42 -0500

    test 123

--- src/patterns/page/ui/index.jsx
+++ src/patterns/page/ui/index.jsx
@@ -20,7 +20,7 @@ export { default as Menu } from './components/menu';
 export { default as Input, ConfirmInput } from './components/input';
 export { default as Dialog } from './components/dialog';
 export { default as Popover } from './components/popover';
-export { default as SelectUI } from './components/selectui';
+export { default as Select } from './components/select';
 export { default as FieldSet } from './components/fieldset';
 
 
