4ac8ffa174c195c12de00abdf718ebd4b9a2d025
shaunaksangdod <shaunaksangdod@gmail.com>
2025-02-26 17:05:52 -0500

    ...

--- src/patterns/page/ui/dataComponents/selector/ComponentRegistry/richtext/theme.js
+++ src/patterns/page/ui/dataComponents/selector/ComponentRegistry/richtext/theme.js
@@ -133,7 +133,7 @@ const theme = {
 
 
     // lexical stuff
-    contentEditable: 'border-none relative pl-6 [tab-size:1] outline-none ',
+    contentEditable: 'border-none relative [tab-size:1] outline-none ',
     editorShell: "font-['Proxima_Nova'] font-[400] text-[16px] text-[#37576B] leading-[22.4px]",
 
     editorScroller: "min-h-[150px] border-0 flex relative outline-0 z-0bh resize-y", //'editor-scroller'
