cdf053e20f5d49685001aa20156fc2ff042e0595
shaunaksangdod <shaunaksangdod@gmail.com>
2025-04-07 13:51:29 -0400

    fix

--- src/patterns/page/ui/dataComponents/selector/ComponentRegistry/shared/filters/Components/RenderFilterValueSelector.jsx
+++ src/patterns/page/ui/dataComponents/selector/ComponentRegistry/shared/filters/Components/RenderFilterValueSelector.jsx
@@ -79,6 +79,7 @@ export const RenderFilterValueSelector = ({
                     if (key === 'allowSearchParams' && value === true && !targetFilter['searchParamKey']) {
                         targetFilter['searchParamKey'] = filterColumn.name;
                     }
+                    draft.readyToLoad = true;
                 });
             }, delay);
         }, [setState]);
--- src/patterns/page/ui/dataComponents/selector/ComponentRegistry/shared/filters/RenderFilters.jsx
+++ src/patterns/page/ui/dataComponents/selector/ComponentRegistry/shared/filters/RenderFilters.jsx
@@ -66,6 +66,7 @@ export const RenderFilters = ({
                             })
                         }
                     });
+                    draft.readyToLoad = true;
                 });
             }
         }, [searchParams, filters]);
