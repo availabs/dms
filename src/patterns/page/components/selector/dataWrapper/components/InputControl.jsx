0bf363eb29148cabbb02c641559c05f446e6d511
shaunaksangdod <shaunaksangdod@gmail.com>
2025-08-07 15:13:23 -0400

    style

--- src/patterns/page/components/selector/dataWrapper/components/InputControl.jsx
+++ src/patterns/page/components/selector/dataWrapper/components/InputControl.jsx
@@ -6,8 +6,8 @@ export const InputControl = ({type, placeHolder, value, setValue, title, classNa
             className={className || `inline-flex w-full justify-center items-center rounded-md px-1.5 py-1 text-sm font-regular 
             text-gray-900 bg-white hover:bg-gray-50 cursor-pointer`}
         >
-            <span className={'flex-1 select-none mr-1'}>{title}</span>
-            <input className={inputClassName || 'p-0.5'}
+            <span className={'flex-1 select-none mr-1 w-fit'}>{title}</span>
+            <input className={inputClassName || 'flex-1 p-0.5 border-b border-0.5'}
                    type={type}
                    placeholder={placeHolder}
                    value={value}
