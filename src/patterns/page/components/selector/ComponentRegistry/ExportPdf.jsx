1ad496d7b86c9977841dd08ffb2ab0ea8e8c3551
sarangp3010 <spatel25@albany.edu>
2025-08-20 12:29:37 -0400

    add select from UI in pdfExport

--- src/patterns/page/components/selector/ComponentRegistry/ExportPdf.jsx
+++ src/patterns/page/components/selector/ComponentRegistry/ExportPdf.jsx
@@ -1,6 +1,5 @@
 import React, { useMemo, useState, useEffect, useRef } from 'react';
 
-//import { Select } from "~/modules/avl-components/src"
 import { PageContext, CMSContext } from '../../../context';
 import { selectablePDF2 } from '../../saveAsPDF/PrintWell/selectablePDF';
 
@@ -58,7 +57,7 @@ function pdfExport({ }) {
     fetchPatterns();
   }, [app, siteType]);
 
-  const type = useMemo(() => selectedPattern?.doc_type, [selectedPattern]); 
+  const type = useMemo(() => selectedPattern?.doc_type, [selectedPattern]);
 
   useEffect(() => {
     if (!app || !type) return;
@@ -348,17 +347,20 @@ function pdfExport({ }) {
       </div>
 
       <div className="mb-4">
-        {/* <label className="block mb-1 font-medium">Select Pattern</label> */}
-          <Select
-            options={patterns}  
-            accessor={o => `${o.name ?? ''} (${o.doc_type ?? ''})`} 
-            value={selectedPattern}
-            onChange={(val) => {
-              setSelectedPageIds(new Set());
-              setExpandedNodeIds(new Set());
-              setSelectedPattern(val)}}
-            placeholder="Select a pattern..."
-          />
+        <Select
+          options={patterns.map(o => ({
+            label: `${o?.name ?? ''} (${o?.doc_type ?? ''})`,
+            value: JSON.stringify(o), 
+          }))}
+          value={selectedPattern ? JSON.stringify(selectedPattern) : ""}
+          onChange={(e) => {
+            const obj = JSON.parse(e.target.value);
+            setSelectedPageIds(new Set());
+            setExpandedNodeIds(new Set());
+            setSelectedPattern(obj); 
+          }}
+          placeholder="Select a pattern..."
+        />
       </div>
       <div className="max-w-xl overflow-auto max-h-[500px] scrollbar-sm">
         {selectedPattern ?
