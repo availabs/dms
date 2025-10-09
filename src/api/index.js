7acaab8c44a8e592d6ccc6dba5e6316527f784b3
shaunaksangdod <shaunaksangdod@gmail.com>
2025-10-09 14:11:15 -0400

    ...

--- src/api/index.js
+++ src/api/index.js
@@ -71,7 +71,8 @@ export async function dmsDataLoader (falcor, config, path='/') {
 		// special routes for 'load', 'uda' action
 		const options = activeConfigs.find(ac => ['udaLength'].includes(ac.action))?.filter?.options;
 		if(options) lengthReq = ['uda', env, 'viewsById', view_id, 'options', options, 'length' ];
-        // await falcor.invalidate(lengthReq)
+        // console.log('invalidating length')
+        await falcor.invalidate(lengthReq)
     }
     let length;
     try{
@@ -103,7 +104,8 @@ export async function dmsDataLoader (falcor, config, path='/') {
         try{
             const udaReqsToInvalidate = newRequests.filter(r => r.includes('uda'));
             if(udaReqsToInvalidate.length){
-                // await falcor.invalidate(udaReqsToInvalidate)
+                // console.log('invalidating index')
+                await falcor.invalidate(udaReqsToInvalidate)
             }
             await falcor.get(...newRequests)
         }catch (e){
