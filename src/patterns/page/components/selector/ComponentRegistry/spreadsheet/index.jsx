688aeb2aa00e84fd2f87c405ecf24c8b9d437cd7
shaunaksangdod <shaunaksangdod@gmail.com>
2025-11-12 12:20:44 -0500

    add new row spreadsheet

--- src/patterns/page/components/selector/ComponentRegistry/spreadsheet/index.jsx
+++ src/patterns/page/components/selector/ComponentRegistry/spreadsheet/index.jsx
@@ -69,6 +69,7 @@ export const RenderTable = ({cms_context, isEdit, updateItem, removeItem, addIte
                   gridRef={gridRef}
                   theme={theme} paginationActive={paginationActive}
                   updateItem={updateItem} removeItem={removeItem}
+                  newItem={newItem} setNewItem={setNewItem} addItem={addItem}
                   numColSize={numColSize} gutterColSize={gutterColSize} frozenColClass={frozenColClass} frozenCols={frozenCols}
                   isActive={isActive}
     />
@@ -155,6 +156,17 @@ export default {
             // settings from more dropdown are stored in state.display
             {type: 'toggle', label: 'Attribution', key: 'showAttribution'},
             {type: 'toggle', label: 'Allow Edit', key: 'allowEditInView'},
+            {type: 'toggle', label: 'Allow Add New', key: 'allowAdddNew'},
+            {type: 'select', label: 'Add New Behaviour', key: 'addNewBehaviour', displayCdn: ({display}) => display.allowAdddNew,
+                options: [
+                    {label: 'Please select', value: ''},
+                    {label: 'Append Entry', value: 'append'},
+                    {label: 'Clear Form', value: 'clear'},
+                    {label: 'Navigate', value: 'navigate'},
+                ]
+            },
+            {type: 'input', inputType: 'text', label: 'Navigate to', key: 'navigateUrlOnAdd',
+                displayCdn: ({display}) => display.allowAdddNew && display.addNewBehaviour === 'navigate'},
             {type: 'toggle', label: 'Use Page Filters', key: 'usePageFilters'},
             {type: 'toggle', label: 'Show Total', key: 'showTotal'},
             {type: 'toggle', label: 'Striped', key: 'striped'},
