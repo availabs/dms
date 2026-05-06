13a0c0878e0d15999743b4ac304bdfd60ffa1145
shaunaksangdod <shaunaksangdod@gmail.com>
2026-05-06 11:19:13 -0400

    multiselect search pinned to top

--- packages/dms/src/ui/columnTypes/multiselect.jsx
+++ packages/dms/src/ui/columnTypes/multiselect.jsx
@@ -19,9 +19,10 @@ const theme = {
         statusWrapper: 'flex items-center p-0.5',
         tokenWrapper: 'w-fit flex m-0.5 px-2 py-1 mx-1 bg-[#C5D7E0] text-[#37576B] hover:bg-[#E0EBF0] rounded-md transition ease-in whitespace-nowrap',
         removeIcon: 'fa fa-xmark px-1 text-xs text-red-500 hover:text-red-600 self-center cursor-pointer transition ease-in',
-        menuWrapper: 'absolute p-2 bg-white min-w-[100px] w-full max-h-[150px] overflow-auto scrollbar-sm shadow-lg z-10 rounded-lg',
-        alwaysOpenMenuWrapper: 'p-2 bg-white min-w-[100px] w-full max-h-[300px] overflow-auto scrollbar-sm z-20 rounded-lg',
-        tabularMenuWrapper: 'flex flex-row flex-wrap p-2 bg-white min-w-[100px] w-full overflow-auto scrollbar-sm z-20 rounded-lg',
+        menuWrapper: 'absolute p-2 bg-white min-w-[100px] w-full shadow-lg z-10 rounded-lg',
+        alwaysOpenMenuWrapper: 'p-2 bg-white min-w-[100px] w-full z-20 rounded-lg',
+        tabularMenuWrapper: 'flex flex-row flex-wrap p-2 bg-white min-w-[100px] w-full z-20 rounded-lg',
+        optionsWrapper: 'max-h-[300px] overflow-auto scrollbar-sm',
         menuItem: 'flex gap-0.5 px-2 py-1 text-sm hover:bg-blue-300 hover:cursor-pointer transition ease-in rounded-md',
         smartMenuWrapper: 'w-full h-full flex flex-wrap',
         smartMenuItem: 'w-fit px-1 py-0.5 m-1 bg-blue-100 hover:bg-blue-300 hover:cursor-pointer transition ease-in border rounded-lg text-xs',
@@ -88,8 +89,9 @@ const RenderMenu = ({
                         onFocus={() => setIsSearching(true)}
                     />
             }
-            {
-                tabular ? null :
+            <div className={theme?.multiselect?.optionsWrapper}>
+                {
+                    tabular ? null :
                     <div className={theme?.multiselect?.smartMenuWrapper}>
                         {
                             [selectAllOption, removeAllOption]
@@ -113,43 +115,44 @@ const RenderMenu = ({
                                     </div>)
                         }
                     </div>
-            }
-            {
-                (options || [])
-                    .filter(o =>
-                        (displayDetailedValues ? !mappedValue.includes(o.value || o) : true) && // if not showing selected values in bar, show them in options
-                        (o.label || o)?.toString()?.toLowerCase().includes(searchKeyword?.toLowerCase()) // filter searched value
-                    )
-                    .map((o, i) => {
-                        const isOptionSelected = mappedValue.includes(o.value || o);
-                        return (
-                            <div
-                                key={`option-${i}`}
-                                className={theme?.multiselect?.menuItem}
-                                onClick={e => {
-                                    // newValue should return .value if available instead of full options mapped obj
-                                    const newValue =
-                                        singleSelectOnly ? (o?.value ?? o) :
-                                            isOptionSelected ? mappedValue.filter(v => (v?.value ?? v) !== (o?.value ?? o)) :
-                                            [...value, o].map(o => o?.value ?? o)
-                                    onChange(newValue);
-                                    singleSelectOnly && !keepMenuOpen && setIsSearching(false);
-                                }}>
-                                {
-                                    !displayDetailedValues && isOptionSelected ?
-                                        <CircleCheck className={theme?.multiselect?.selectedValueIcon || 'size-4'} /> : null
-                                }
-                                {meta?.[o.label || o] ?? o.label ?? o}
-                            </div>
-                        )
-                    })
-            }
-            {loading && (
-                <div className="flex items-center justify-center gap-1 py-1 text-xs text-gray-400">
-                    <div className="size-3 border-2 border-gray-300 border-t-blue-500 rounded-full animate-spin" />
-                    loading...
-                </div>
-            )}
+                    }
+                    {
+                        (options || [])
+                            .filter(o =>
+                                (displayDetailedValues ? !mappedValue.includes(o.value || o) : true) && // if not showing selected values in bar, show them in options
+                                (o.label || o)?.toString()?.toLowerCase().includes(searchKeyword?.toLowerCase()) // filter searched value
+                            )
+                            .map((o, i) => {
+                                const isOptionSelected = mappedValue.includes(o.value || o);
+                                return (
+                                    <div
+                                        key={`option-${i}`}
+                                        className={theme?.multiselect?.menuItem}
+                                        onClick={e => {
+                                            // newValue should return .value if available instead of full options mapped obj
+                                            const newValue =
+                                                singleSelectOnly ? (o?.value ?? o) :
+                                                    isOptionSelected ? mappedValue.filter(v => (v?.value ?? v) !== (o?.value ?? o)) :
+                                                        [...value, o].map(o => o?.value ?? o)
+                                            onChange(newValue);
+                                            singleSelectOnly && !keepMenuOpen && setIsSearching(false);
+                                        }}>
+                                        {
+                                            !displayDetailedValues && isOptionSelected ?
+                                                <CircleCheck className={theme?.multiselect?.selectedValueIcon || 'size-4'} /> : null
+                                        }
+                                        {meta?.[o.label || o] ?? o.label ?? o}
+                                    </div>
+                                )
+                            })
+                    }
+                    {loading && (
+                        <div className="flex items-center justify-center gap-1 py-1 text-xs text-gray-400">
+                            <div className="size-3 border-2 border-gray-300 border-t-blue-500 rounded-full animate-spin" />
+                            loading...
+                        </div>
+                    )}
+            </div>
         </div>
     )
 }
